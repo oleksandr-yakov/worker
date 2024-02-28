ee3bbc73705c1f08dde0e4b35f994a8243a6d19b
alex-yakov <oyakovenko@oversecured.com>
2024-02-28 14:54:43 +0200

    commit the arm saas worker

--- GetTag.py
+++ GetTag.py
@@ -29,8 +29,7 @@ def find_max_tag(repository_name, token):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print(False)
-        sys.exit(0)
+        sys.exit(1)
 
     option = sys.argv[1]
     argument = sys.argv[2]
